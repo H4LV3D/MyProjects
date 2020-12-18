fe59da908f277078bf1dd8fab9ec9e020f7fab5e
Toluwalope Akinkunmi <64863887+dynamix23@users.noreply.github.com>
2020-12-18 10:56:36 +0000

    uploaded edits

---
+++ index.js
@@ -0,0 +1,22 @@
+//Javascript for index.js and styles.css
+
+window.alert(40);
+
+
+
+//SELECTORS
+
+
+
+//EVENTLISTNERS
+
+
+
+//FUNCTIONS
+
+
+
+//EXTRAS
+var loader = document.body.getElementsByClassName('load');
+loader.delay(7000).fadeOut(slow);
+alert(10);
