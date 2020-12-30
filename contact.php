51dbd99f69e7c53a98ef1e91b1f95da6544f28a1
Toluwalope Akinkunmi <64863887+dynamix23@users.noreply.github.com>
2020-12-30 07:31:36 +0000

    fresh commits

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+    echo "Hello world";
+    $full = "favour"
+    define($pi, 3.142);
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+</head>
+<body>
+   
+
+ 
+</body>
+</html>
---
+++ index.js
@@ -0,0 +1,56 @@
+//Javascript for index.js and styles.css
+window.alert(10);
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per + '%');
+    $({animatedvalue: 0}).animate({animatedvalue: per},{
+        duration: 1000,
+        step: function(){
+           $this.attr('per', Math.floor(this.animatedvalue) + '%'); 
+        },
+        complete: function(){
+            $this.attr('per', Math.floor(this.animatedvalue) + '%'); 
+        }
+    });
+});
+
+
+
+
+
+
+//SELECTORS
+
+
+
+//EVENTLISTNERS
+
+
+//FUNCTIONS
+
+
+
+//EXTRAS
+var loader = document.body.getElementsByClassName('load');
+loader.delay(7000).fadeOut(slow);
+window.alert(10);
+
+
+
+
+
+
+const logo = document.getElementsByClassName('.logo');
+logo.color = red;
+
+
+
+//FUNCTIONS
+
+
+
+//EXTRAS
+var loader = document.body.getElementsByClassName('load');
+loader.delay(7000).fadeOut(slow);
+alert(10);
