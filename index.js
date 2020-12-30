859554e1d47696a30dc04934ed5e7e67f8a8ffb7
Toluwalope Akinkunmi <64863887+dynamix23@users.noreply.github.com>
2020-12-30 05:44:43 +0000

    total commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 //Javascript for index.js and styles.css
-// window.alert(10);
-//window.alert(40);
-$(window).load(function(){
-$(".load").animate({
-    top: -1000
-}, 1500); 
+window.alert(10);
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per + '%');
+    $({animatedvalue: 0}).animate({animatedvalue: per},{
+        duration: 1000,
+        step: function(){
+           $this.attr('per', Math.floor(this.animatedvalue) + '%'); 
+        },
+        complete: function(){
+            $this.attr('per', Math.floor(this.animatedvalue) + '%'); 
+        }
+    });
 });
+
+
+
+
 
 
 //SELECTORS
@@ -15,7 +27,6 @@ $(".load").animate({
 //EVENTLISTNERS
 
 
-
 //FUNCTIONS
 
 
@@ -23,4 +34,4 @@ $(".load").animate({
 //EXTRAS
 var loader = document.body.getElementsByClassName('load');
 loader.delay(7000).fadeOut(slow);
-alert(10);
+window.alert(10);
